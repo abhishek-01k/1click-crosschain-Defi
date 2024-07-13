8b725ff5e6b68e46fd0f090e15bb566ec078c7c6
kamalbuilds <geniusamansingh@gmail.com>
2024-07-14 00:48:29 +0530

    swapkit integration

---
+++ lib/client.ts
@@ -0,0 +1,51 @@
+import { SwapKit } from "@swapkit/core";
+import { MayachainPlugin, ThorchainPlugin } from "@swapkit/plugin-thorchain";
+import { ChainflipPlugin } from "@swapkit/plugin-chainflip";
+import { evmWallet } from "@swapkit/wallet-evm-extensions";
+import { coinbaseWallet } from "@swapkit/wallet-coinbase";
+import { keplrWallet } from "@swapkit/wallet-keplr";
+import { keystoreWallet } from "@swapkit/wallet-keystore";
+import { talismanWallet } from "@swapkit/wallet-talisman";
+import { walletconnectWallet } from "@swapkit/wallet-wc";
+import { xdefiWallet } from "@swapkit/wallet-xdefi";
+
+export type SwapKitClient = ReturnType<typeof SwapKit>;
+
+const plugins = {
+  ...ChainflipPlugin,
+  ...MayachainPlugin,
+  ...ThorchainPlugin,
+};
+
+const wallets = {
+  ...evmWallet,
+  ...coinbaseWallet,
+  ...keplrWallet,
+  ...keystoreWallet,
+  ...talismanWallet,
+  ...walletconnectWallet,
+  ...xdefiWallet,
+};
+
+
+const clientCache = new Map<string, SwapKitClient>();
+
+const swapKitParams = {
+  ethplorerApiKey: 'freekey',
+  covalentApiKey: '',
+  blockchairApiKey: '',
+  walletConnectProjectId: '',
+  stagenet: false,
+};
+
+export const getSwapKitClient = (
+) => {
+  const key = JSON.stringify(swapKitParams);
+  if (clientCache.has(key)) return clientCache.get(key);
+
+  const client = SwapKit({ ...swapKitParams, wallets, plugins });
+
+  clientCache.set(key, client);
+
+  return client;
+};
