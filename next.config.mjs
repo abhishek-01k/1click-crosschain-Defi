0f66acd3e66dce82185126ac3558d85c68719ebd
kamalbuilds <geniusamansingh@gmail.com>
2024-07-29 01:30:26 +0530

    fix patch4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,58 @@
+import { createRequire } from "module";
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+
+  webpack: (config, { isServer, webpack }) => {
+    if (!isServer) {
+      config.plugins.push(new NodePolyfillPlugin());
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          global: [require.resolve("global"), "default"],
+          process: "process/browser",
+        }),
+      );
+
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          "global.crypto": "crypto",
+          "global.msCrypto": "crypto",
+          "global.process": "process",
+          "global.Uint8Array": JSON.stringify(Uint8Array),
+        }),
+      );
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        path: require.resolve("path-browserify"),
+        process: require.resolve("process/browser"),
+        stream: require.resolve("stream-browserify"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify/browser"),
+      };
+    }
+
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      topLevelAwait: true,
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
